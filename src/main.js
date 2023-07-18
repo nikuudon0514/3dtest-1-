834e2f594f2c943e7e70165688c2f89ce8744709
nikuudon0514 <59128525+nikuudon0514@users.noreply.github.com>
2023-07-18 19:36:23 +0900

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/mZQiMt1M56pHMDJ2/scene.splinecode');
